7851417552c34909c433a6eb9bc83e71efd55a64
JT Yim <JT@JTYim.com>
2016-01-15 14:23:37 -0500

    completed tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,27 +1,27 @@
 var Tree = function(value){
   var newTree = {};
   newTree.value = value;
-
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
-
-
-
-
 var treeMethods = {};
 
+// O(1)
 treeMethods.addChild = function(value){
-
+  this.children.push(Tree(value));
 };
 
+// O(n)
 treeMethods.contains = function(target){
-
+  if( this.value===target ){ return true; }
+  return _.reduce(this.children, function(acc, childNode){
+    if(acc){ return true; }
+    return childNode.contains(target);
+  }, false);
 };
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
