b7031bd82b4596f006eb0aefae130543acdf43cb
JT Yim <JT@JTYim.com>
2016-01-16 18:07:27 -0500

    complete bin tree; refactor all

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,59 @@
 var BinarySearchTree = function(value){
+  var x = Object.create(BinarySearchTree.prototype);
+  x.value = value;
+  x.left = null;
+  x.right = null;
+  return x;
+};
+
+var TreeNode = function(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+}
+
+BinarySearchTree.prototype.insert = function(value) {
+  var newNode=new TreeNode(value);
+  if(this.value===null) {
+    this.value = value;
+  }else{
+    insertNode(this, newNode);
+  }
+
+  function insertNode(node, newNode) {
+    if (newNode.value < node.value) {
+      node.left===null ?  node.left=newNode : insertNode(node.left, newNode);
+    }else{
+      node.right===null ? node.right=newNode : insertNode(node.right, newNode);
+    }
+  }
+
+};
+
+BinarySearchTree.prototype.contains = function(value) {
+  var found=false;
+  search(this);
+  function search(node) {
+    if(node.value > value && node.left){
+      search( node.left );
+    }else if(node.value < value && node.right){
+      search( node.right );
+    }
+    node.value===value && ( found=true );
+  }
+  return found;
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+  traverse(this);
+  function traverse(node) {
+    cb(node.value);
+    node.left && (traverse(node.left));
+    node.right && (traverse(node.right));
+  }
 };
 
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
+
+
+
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,23 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = new HashTable();
   return set;
 };
-
 var setPrototype = {};
 
+// O(1)
 setPrototype.add = function(item){
+  this._storage.insert(item,'undefined');
 };
 
+// O(1)
 setPrototype.contains = function(item){
+  return this._storage.retrieve(item) ? true : false;
 };
 
+// O(1)
 setPrototype.remove = function(item){
+  delete this._storage.remove(item);
 };
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
+/*Complexity: What is the time complexity of the above functions? */
